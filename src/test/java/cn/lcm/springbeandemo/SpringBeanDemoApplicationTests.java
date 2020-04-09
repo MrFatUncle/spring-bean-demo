ed46b08aa90c0cd90bd97e8716b1c11f7f6f1415
liaocm <122789454@qq.com>
2020-04-09 15:54:50 +0800

    first commit

---
+++ src/test/java/cn/lcm/springbeandemo/SpringBeanDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.lcm.springbeandemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeanDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
